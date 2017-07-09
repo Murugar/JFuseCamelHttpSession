98bd5dfa554f856a64b9b541ce277702c5ba76a8
Davanon <davanon2014@gmail.com>
2017-07-09 18:04:06 +0530

    first commit

---
+++ src/main/java/com/iqmsoft/camel/session/MyProcessor.java
@@ -0,0 +1,15 @@
+package com.iqmsoft.camel.session;
+
+import javax.servlet.http.HttpSession;
+
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class MyProcessor implements Processor {
+
+	public void process(Exchange exchange) throws Exception {
+	    exchange.getProperty("session",HttpSession.class).setAttribute("logged", true);
+	}
+
+}
